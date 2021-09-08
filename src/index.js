f985b75bd006a6e48e7f5171a561843da7785662
felipe reyel <felipe.reyel@gmail.com>
2021-09-08 20:18:57 -0300

    add CRUD

--- src/index.js
+++ src/index.js
@@ -3,10 +3,53 @@ import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3000;
+const buckets = {};
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("hello"));
+const userMid = (req, res, next) => {
+	if (!req.headers.bucket) {
+		res.status(400).send("missing bucket");
+	} else {
+    if (!buckets[req.headers.bucket]) buckets[req.headers.bucket] = [];
+		res.locals.bucket = buckets[req.headers.bucket];
+		next();
+	}
+};
+
+app.put("/", userMid, (req, res) => {
+	res.locals.bucket.push(req.body);
+	res.send(res.locals.bucket);
+});
+
+app.get("/", userMid, (req, res) => {
+	res.send(res.locals.bucket);
+});
+
+app.get("/:id", userMid, (req, res) => {
+	res.send(res.locals.bucket[req.params.id] || null);
+});
+
+app.post("/:id", userMid, (req, res) => {
+	if (res.locals.bucket[req.params.id]) {
+		Object.assign(res.locals.bucket[req.params.id], req.body);
+		res.send(res.locals.bucket);
+	} else {
+		res.status(400).send("id does not exist");
+	}
+});
+
+app.delete("/:id", userMid, (req, res) => {
+	if (res.locals.bucket[req.params.id]) {
+		res.locals.bucket = res.locals.bucket.filter(
+			(e, i) => i != req.params.id
+		);
+		res.send(res.locals.bucket);
+	} else {
+		res.status(400).send("id does not exist");
+	}
+});
+
 
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
